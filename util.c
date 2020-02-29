17e4e08013b19ad754c97507fd341cb1e266d2de
lvxinliang <science_lxl@163.com>
2020-02-29 22:57:18 +0800

    修改WiFi连接方式

--- util.c
+++ util.c
@@ -35,19 +35,50 @@ int get_wlan_address(char *address)
 
 int connect_wifi_by_str(char *str)
 {
-    int i = 0;
-    for (i = 0; i < strlen(str); i++)
-    {
-        if (str[i] == ',')
-        {
-            str[i] = ' ';
-            break;
-        }
-    }
     char cmd[256] = {0};
-    sprintf(cmd, "connect_wifi.sh %s", str);
-    printf("connect wifi cmd:%s \n", cmd);
-    return system(cmd);
+    char *revbuf[8] = {0}; //存放分割后的子字符串
+    int num = 0;
+    split(str, ",", revbuf, &num);
+    sprintf(cmd, "wpa_cli -i wlan0 set_network 0 ssid \\\"%s\\\"", revbuf[0]);
+    printf("cmd:%s\n", cmd);
+    system(cmd);
+    sprintf(cmd, "wpa_cli -i wlan0 set_network 0 psk \\\"%s\\\"", revbuf[1]);
+    printf("cmd:%s\n", cmd);
+    system(cmd);
+    sprintf(cmd, "wpa_cli -i wlan0 save_config");
+    printf("cmd:%s\n", cmd);
+    system(cmd);
+    sprintf(cmd, "wpa_cli -i wlan0 enable_network 0");
+    printf("cmd:%s\n", cmd);
+    system(cmd);
+    return 0;
+}
+
+void split(char *src, const char *separator, char **dest, int *num)
+{
+    /*
+        src 源字符串的首地址(buf的地址) 
+        separator 指定的分割字符
+        dest 接收子字符串的数组
+        num 分割后子字符串的个数
+    */
+    char *pNext;
+    int count = 0;
+
+    if (src == NULL || strlen(src) == 0) //如果传入的地址为空或长度为0，直接终止
+        return;
+
+    if (separator == NULL || strlen(separator) == 0) //如未指定分割的字符串，直接终止
+        return;
+
+    pNext = (char *)strtok(src, separator); //必须使用(char *)进行强制类型转换(虽然不写有的编译器中不会出现指针错误)
+    while (pNext != NULL)
+    {
+        *dest++ = pNext;
+        ++count;
+        pNext = (char *)strtok(NULL, separator); //必须使用(char *)进行强制类型转换
+    }
+    *num = count;
 }
 
 #endif
--- util.h
+++ util.h
@@ -14,4 +14,6 @@
 int get_wlan_address(char *address);
 
 int connect_wifi_by_str(char *str);
+
+void split(char *src, const char *separator, char **dest, int *num);
 #endif
